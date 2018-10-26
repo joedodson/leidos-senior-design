6c9673cd927a71a29f3f0d14621ed151ddd04a43
Brian <brian.rawlings@ucdenver.edu>
2018-10-26 00:01:04 -0600

    Added wrapper functions, and changed the button to use the wrapper to turn on the engine.

--- DroneControllerApp/app/src/main/java/com/leidossd/dronecontrollerapp/SecondActivity.java
+++ DroneControllerApp/app/src/main/java/com/leidossd/dronecontrollerapp/SecondActivity.java
@@ -5,28 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import com.dji.sdk.sample.internal.utils.ModuleVerificationUtil;
+import com.leidossd.djiwrapper.FlightControllerWrapper;
+
+import dji.sdk.flightcontroller.FlightController;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private int testValue = 0;
+    private boolean engineOn = false;
     private TextView testText;
+    private FlightControllerWrapper fcw;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         testText = findViewById(R.id.text_1);
-        testText.setText(String.valueOf(testValue));
+        testText.setText("Off");
     }
 
     public void onClicked(View view) {
+        fcw = new FlightControllerWrapper();
+
         switch (view.getId()) {
-            case(R.id.button_1): { //+ Button
-                testText.setText(String.valueOf(++testValue));
+            case(R.id.button_1): { //On Button
+                fcw.turnOnMotors(null);
+                engineOn = true;
+                testText.setText("On");
                 break;
             }
-            case(R.id.button_2): { //- Button
-                testText.setText(String.valueOf(--testValue));
+            case(R.id.button_2): { //Off Button
+                fcw.turnOffMotors(null);
+                engineOn = false;
+                testText.setText("Off");
                 break;
             }
             case(R.id.button_3): { //Back button
