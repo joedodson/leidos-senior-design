55640fa2aa707d767115c9c9cedc9bcdf778e0d9
Dalton Burke <dmtburke@gmail.com>
2018-11-01 17:14:52 -0400

    some progress in deadreckoning, working on making it understand the direction that it's facing

--- DroneControllerApp/app/src/main/java/com/leidossd/djiwrapper/FlightControllerWrapper.java
+++ DroneControllerApp/app/src/main/java/com/leidossd/djiwrapper/FlightControllerWrapper.java
@@ -1,10 +1,13 @@
 package com.leidossd.djiwrapper;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import java.lang.Math;
 
 import dji.common.error.DJIError;
 import dji.common.flightcontroller.virtualstick.RollPitchControlMode;
 import dji.common.flightcontroller.virtualstick.VerticalControlMode;
 import dji.common.flightcontroller.virtualstick.FlightControlData;
+import dji.common.flightcontroller.virtualstick.YawControlMode;
 import dji.common.util.CommonCallbacks;
 import dji.sdk.flightcontroller.FlightController;
 import dji.sdk.sdkmanager.DJISDKManager;
@@ -14,9 +17,14 @@ public class FlightControllerWrapper {
     private static FlightControllerWrapper instance = null;
 
     private FlightController flightController;
-    private float xPos;
-    private float yPos;
-    private float zPos;
+    private FlightData flightData;
+
+    private Coordinate position;
+    private float directionFacing;
+
+    private boolean rotationLock;
+
+    private float flightSpeed;
 
     public static FlightControllerWrapper getInstance(){
         if(instance == null)
@@ -28,41 +36,121 @@ public class FlightControllerWrapper {
     private FlightControllerWrapper(){
         flightController = ((Aircraft) DJISDKManager.getInstance().
                                        getProduct()).getFlightController();
-        xPos = 0;
-        yPos = 0;
-        zPos = 0;
+        this.position = new Coordinate(0,0,0);
+        this.rotationLock = true;
+        this.directionFacing = 0;
+        this.flightData = null;
+        this.flightSpeed = (float) 0.5;
     }
 
-    public void gotoX(float x){
-        flightController.setRollPitchControlMode(RollPitchControlMode.VELOCITY);
+    public Coordinate getPosition(){
+        if(!isInFlight())
+            return position;
 
+        // return where it currently is in the flight path
+        return flightData.currentPosition();
     }
 
-    public void gotoY(float y){
-        flightController.setRollPitchControlMode(RollPitchControlMode.VELOCITY);
-
+    public boolean isInFlight(){
+        return flightData != null;
     }
 
-    public void gotoZ(float z){
-        flightController.setVerticalControlMode(VerticalControlMode.POSITION);
-        float pitch = 0;
-        float roll = 0;
-        float yaw = 0;
-        float verticalThrottle = 0;
-
+    public void rotateTo(float angle){
+        flightController.setYawControlMode(YawControlMode.ANGLE);
         flightController.sendVirtualStickFlightControlData(
-                        new FlightControlData(pitch, roll, yaw, verticalThrottle),
-                        new CommonCallbacks.CompletionCallback() {
+                new FlightControlData(0,0,0,angle),
+                new CommonCallbacks.CompletionCallback() {
                             @Override
                             public void onResult(DJIError djiError) {
                             }
                         });
     }
 
-    public void gotoXYZ(float x, float y, float z){
-        gotoX(x);
-        gotoY(y);
-        gotoZ(z);
+    public void goToRelativeXYZ(Coordinate destination){
+
+    }
+
+
+    public void goToAbsoluteXYZ(Coordinate destination){
+        flightController.setRollPitchControlMode(RollPitchControlMode.VELOCITY);
+        flightController.setVerticalControlMode(VerticalControlMode.VELOCITY);
+
+        Coordinate movement;
+        float roll;
+        float pitch;
+        float throttle;
+
+
+        movement = destination.add(position.scale(-1));
+
+        // distinguish between rotation lock and not
+        if(rotationLock) {
+        }
+        else{
+            // rotate to face the point, then straight to it
+
+            movement = new Coordinate(
+                           0,
+                           new Coordinate(movement.getX(), movement.getY(), 0).magnitude(),
+                           movement.getZ());
+        }
+
+        double flightTime = movement.magnitude()/flightSpeed;
+
+        roll = (float) (movement.getX()/flightTime);
+        pitch = (float) (movement.getY()/flightTime);
+        throttle = (float) (movement.getZ()/flightTime);
+
+
+        // Start up the engines going at speed
+
+        flightController.sendVirtualStickFlightControlData(
+                        new FlightControlData(pitch, roll , 0, throttle),
+                        new CommonCallbacks.CompletionCallback() {
+                            @Override
+                            public void onResult(DJIError djiError) {
+                            }
+                        });
+
+        // Stop the motion after enough time has passed
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                        halt();
+            }
+        }, (long) (flightTime*1000));
+
+
+        // We will need to take into account "skidding" as the drone stops and overshoots the target.
+        // Which we'll need to test manually, probably using tape on the floor and the camera
+    }
+
+
+    // Stop the current flight, set all velocities to zero
+    public void halt(){
+        flightController.setRollPitchControlMode(RollPitchControlMode.VELOCITY);
+        flightController.setVerticalControlMode(VerticalControlMode.VELOCITY);
+
+        // Check timer and make sure to update position
+
+        position = getPosition();
+        flightData = null;
+
+        flightController.sendVirtualStickFlightControlData(
+                        new FlightControlData(0,0,0,0),
+                        new CommonCallbacks.CompletionCallback() {
+                            @Override
+                            public void onResult(DJIError djiError) {
+                            }
+                        });
+
+    }
+
+
+    public void goToOrigin(){
+        goToAbsoluteXYZ(new Coordinate(0,0,0));
     }
 
 
