9088fc80665f846bcae611ccbb0aefed1df66ea7
Joseph Dodson <joseph.dodson@ucdenver.edu>
2019-02-02 16:55:04 -0700

    Grid parent implemented, tested with stub fragment

--- DroneControllerApp/app/src/main/java/com/leidossd/dronecontrollerapp/GridParentActivity.java
+++ DroneControllerApp/app/src/main/java/com/leidossd/dronecontrollerapp/GridParentActivity.java
@@ -1,13 +1,62 @@
 package com.leidossd.dronecontrollerapp;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class GridParentActivity extends AppCompatActivity {
+public class GridParentActivity extends AppCompatActivity implements GridFragment.fragmentInteractionListener {
+
+    FragmentManager fragmentManager;
+    GridFragment gridFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_parent);
+
+        fragmentManager = getSupportFragmentManager();
+        gridFragment = new GridFragment();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.grid_fragment_container, gridFragment)
+                .show(gridFragment)
+                .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent upIntent = NavUtils.getParentActivityIntent(this);
+                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+                    // This activity is NOT part of this app's task, so create a new task
+                    // when navigating up, with a synthesized back stack.
+                    TaskStackBuilder.create(this)
+                            // Add all of this activity's parents to the back stack
+                            .addNextIntentWithParentStack(upIntent)
+                            // Navigate up to the closest parent
+                            .startActivities();
+                } else {
+                    // This activity is part of this app's task, so simply
+                    // navigate up to the logical parent activity.
+                    NavUtils.navigateUpTo(this, upIntent);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendInput(String s) {
+        showToast("GridParent received: " + s);
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
